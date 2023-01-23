19601133bd780e5b73d3bf3defa4b1d29f62c3c6
Dread2 <julianeatong2@gmail.com>
2023-01-22 20:25:12 -0800

    Setup multiple preprocessors

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,23 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-auto'
+// Enable use of PostCSS in <style> blocks
+import { vitePreprocess } from '@sveltejs/kit/vite'
+// Enable use of React components in Svelte
+import preprocessReact from 'svelte-preprocess-react/preprocessReact'
+import preprocess from 'svelte-preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter()
-	}
-};
+	},
+	preprocess: [
+		vitePreprocess(),
+		preprocessReact(),
+		preprocess({
+			sourceMap: true,
+			postcss: true
+		})
+	]
+}
 
-export default config;
+export default config
