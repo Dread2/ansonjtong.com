d68912abe1bab0a2c4ad770165e51c1461e3ff3c
Dread2 <julianeatong2@gmail.com>
2023-01-22 20:33:37 -0800

    Almost done developing the front page. Need to add more CSS, HTML, and JSS

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,44 @@
+<script>
+
+</script>
+<!--Hack to preload fonts -->
+<head>
+	<link rel="stylesheet" href="/fonts/ErvaEsra.woff2" as="font" type="font/woff2" crossorigin="anonymous" />
+</head>
+
+<body>
+	<script>
+		// Dynamically set title of tab to URL
+		window.onload = function () {
+			var pageTitle = document.title
+
+			document.addEventListener('visibilitychange', function (e) {
+				var isPageActive = !document.hidden
+
+				if (!isPageActive) {
+					document.title = pageTitle
+				}
+			})
+		}
+	</script>
+	<main>
+		<h1>ANSON TONG</h1>
+	</main>
+</body>
+
+<style>
+	/* Set custom font */
+	@font-face {
+		font-family: 'ErvaEsra', 'sans-serif';
+		font-style: normal;
+		font-weight: 400;
+		src: url(fonts/ErvaEsra.woff2), url(fonts/ErvaEsra.ttf);
+		font-display: swap;
+	}
+	/* Set values we can reuse on root selector. */
+	:root {
+		--primary: #3df5ff;
+		--dark: #000000;
+		--light: #eeeeee;
+	}
+</style>
